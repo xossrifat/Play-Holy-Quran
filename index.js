79f2281146f0781e4bc75cf1299aa5ed95fd6694
xossrifat <151685749+xossrifat@users.noreply.github.com>
2024-12-25 01:17:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-aapp.get('/', (req, res) => res.send('Bot is running!'));
+app.get('/', (req, res) => res.send('Bot is running!'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
