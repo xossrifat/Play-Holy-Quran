a80047053663ed5a31cd010118c8ea2741450309
xossrifat <151685749+xossrifat@users.noreply.github.com>
2024-12-25 17:37:53 +0600

    Update keep-alive.js

--- keep-alive.js
+++ keep-alive.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Bot is running!');
+        res.end('Bot is alive');
     } else {
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('Not Found');
